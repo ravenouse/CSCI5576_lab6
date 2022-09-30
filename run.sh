94b7c2fec3dfbd29b2f7f4640ea2c8c0a6fb0e23
ravenouse <zhwa3087@colorado.edu>
2022-09-30 14:16:05 -0600

    add some required files

---
+++ run.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --time=00:20:00
+#SBATCH --partition=shas-testing
+#SBATCH --output=ex01-%j.out
+
+module purge
+module load intel
+module load impi
+
+module load python/3.6.5
+
+#  This "use" command is required for all of the module loads that follow:
+
+module use /projects/scru5660/public/software/module
+
+# To run our own, new version of valgrind:
+
+module load valgrind-intel
+
+# To run lstopo:
+
+module load hwloc
+
+# To run gnuplot:
+
+module load gnuplot
+
+# To run kcachegrind:
+ 
+module load kcachegrind
+
+# To run Intel Advisor
+
+module use /projects/scru5660/public/software/module
+
+ml intel_advisor
+
+
+
+# Run the application
+./run_case  10 1
